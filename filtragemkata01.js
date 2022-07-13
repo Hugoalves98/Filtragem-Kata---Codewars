19ddfaadb5328a4dc753fd63d28e6dc5264390b7
Hugo Rocha <hugorochalves@.gmail.com>
2022-07-13 11:26:06 -0300

    Primeiro de JS

---
+++ filtragemkata01.js
@@ -0,0 +1,5 @@
+//não pode simplesmente testar como != ''; isso me deixou perplexo no começo e depois me lembrei do operador typeof :)
+function  filter_list ( l )  {
+    // Retorna um novo array com as strings filtradas
+  return  l . filter (  str  =>  typeof  str  !=  'string' ) ;
+}
