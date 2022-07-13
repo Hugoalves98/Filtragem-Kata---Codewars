37e27b4fa09007eb6b2036046cd2d2090548fc33
Hugo Rocha <101724711+Hugoalves98@users.noreply.github.com>
2022-07-13 11:31:29 -0300

    Update filtragemkata01.js

--- filtragemkata01.js
+++ filtragemkata01.js
@@ -3,7 +3,7 @@ function  filter_list ( l )  {
     // Retorna um novo array com as strings filtradas
   return  l . filter (  str  =>  typeof  str  !=  'string' ) ;
 }
-
+ ou esse
 function filter_list(l) {
  return l.filter(v => typeof v == "number")
 }
